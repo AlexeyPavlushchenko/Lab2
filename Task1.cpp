7f5289d23f3afbc31e673ef43efc036eed928df1
Alexey.Pavlushchenko <alex10521@mail.ru>
2015-09-13 06:11:07 +0400

    Tasks 1,2

---
+++ Task1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>;
+using namespace std;
+
+double sinus(double x, double e)
+{
+	bool check = true;
+	double temp = x; 
+	double res = temp;
+	for (double i = 0; check == true ; i++)
+	{
+		temp *= -x*x / ((2 * i + 2)*(2 * i + 3));
+		res += temp;
+		if (((sin(x) + e) >= res) && ((sin(x) - e) <= res))
+		{
+			check = false;
+		}
+	}
+	return res;
+
+}
+int main()
+{
+	double x,e;
+	cout << "Enter x and e" << endl;
+	cin >> x >> e;
+	cout << "sin(x) = " << sinus(x,e);
+	system ("pause");
+}
---
+++ Task2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>;
+using namespace std;
+
+int zero(int N)
+{
+	int counter = 0, a;
+	for (int i = 1; i <= N; i++)
+	{
+		a = i;
+		while ((i % 5) == 0)
+		{
+			counter++;
+			i /= 5;
+		}
+		i = a;
+	}
+	return counter;
+}
+
+int main()
+{
+	int N;
+	cout << " Enter N" << endl;
+	cin >> N;
+	cout << " Number of zeros in the end of N! is " << zero(N) << endl;
+	system("pause");
+}
